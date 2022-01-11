d230231942018a199e0039ffeccc90622248152e
Stain-h <sangheon1646@naver.com>
2022-01-11 09:12:26 +0900

    Feat: Firebase Auth Set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "Router";
 import { auth } from 'fbinstance'
+import { onAuthStateChanged } from "@firebase/auth";
 
 function App() {
   const user = auth.currentUser;
   const [isLogin, setIsLogin] = useState(user);
+  useEffect(() => {
+    onAuthStateChanged(auth, () => console.log(user))
+  }, [])
   return (
     <>
       <Router isLogin={isLogin} />
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
+import { auth } from 'fbinstance';
+import React, { useState } from 'react'
 
 export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    if(name === 'email'){
+      setEmail(value);
+    }else if(name === 'password'){
+      setPassword(value)
+    }
+  }
+  const onHandleSubmit = (event) => {
+    event.preventDefault();
+    let data
+    if(newAccount){
+      data = createUserWithEmailAndPassword(auth, email, password)
+    }else {
+      data = signInWithEmailAndPassword(auth, email, password)
+    }
+  }
+
   return (
-    <div>
-      Auth
-    </div>
+    <>
+      <form onSubmit={onHandleSubmit}>
+        <input type="email" name="email" placeholder="Email" value={email} onChange={onChange} />  
+        <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} />
+        <input type="submit" value={newAccount ? "Create Account" :"Log in"}/>
+      </form> 
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </>
   )
 }
